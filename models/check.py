1b803e6df02844d842dd943bc675543c6464b7d0
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2024-11-17 13:31:59 +0800

    Update check.py

--- models/check.py
+++ models/check.py
@@ -2,8 +2,8 @@ import torch
 from ..validate import anime_rating_score
 
 
-def nsfw_threshold(pil, threshold):
-    if float(threshold) <= float(anime_rating_score(pil)["r18"]):
+def nsfw_threshold(pil, threshold, type_choose):
+    if float(threshold) <= float(anime_rating_score(pil)[str(type_choose)]):
         # Create a black image
         width, height = 256, 256  # The width and height of the image
         black_tensor = torch.zeros(1, height, width)
