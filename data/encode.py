3e8d939934a958832950816841f11bcf3ff502e8
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2024-11-18 11:27:40 +0800

    Update encode.py

--- data/encode.py
+++ data/encode.py
@@ -22,31 +22,6 @@ def rgb_encode(image: ImageTyping, order_: str = 'CHW', use_float: bool = True) 
     :param order_: Order of encoding, default is ``CHW``.
     :param use_float: Use float to represent the channels, default is ``True``. ``np.uint8`` will be used when false.
     :return: Encoded rgb image.
-
-    Examples::
-        >>> from PIL import Image
-        >>> from imgutils.data import rgb_encode
-        >>>
-        >>> image = Image.open('custom_image.jpg')
-        >>> image
-        <PIL.JpegImagePlugin.JpegImageFile image mode=RGB size=1606x1870 at 0x7F9EC37389D0>
-        >>>
-        >>> data = rgb_encode(image)
-        >>> data.shape, data.dtype
-        ((3, 1870, 1606), dtype('float32'))
-        >>> data = rgb_encode(image, order_='CHW')
-        >>> data.shape, data.dtype
-        ((3, 1870, 1606), dtype('float32'))
-        >>> data = rgb_encode(image, order_='WHC')
-        >>> data.shape, data.dtype
-        ((1606, 1870, 3), dtype('float32'))
-        >>> data = rgb_encode(image, use_float=False)
-        >>> data.shape, data.dtype
-        ((3, 1870, 1606), dtype('uint8'))
-
-    .. note::
-        The function :func:`rgb_encode`'s result is the same as \
-            ``torchvision.transforms.functional import to_tensor``'s result when the given ``image`` is in RGB mode.
     """
     image = load_image(image, mode='RGB')
     array = np.asarray(image)
