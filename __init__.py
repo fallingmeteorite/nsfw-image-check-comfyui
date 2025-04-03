18b37caf36c0887dd32c13ad4e6a7477c5118a07
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-04-03 16:24:23 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
-from .nsfw_image_check import NsfwCheckNode
+from .nsfw_image_check import NsfwCheckNode, NsfwAreaCoverNode
 
 NODE_CLASS_MAPPINGS = {
-    "NsfwCheckNode": NsfwCheckNode
+    "NsfwCheckNode": NsfwCheckNode, "NsfwAreaCoverNode": NsfwAreaCoverNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "NsfwCheckNode": "Nsfw Image Check Node"
+    "NsfwCheckNode": "Nsfw Image Check Node",
+    "NsfwAreaCoverNode": "Nsfw area coverage",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
